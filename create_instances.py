d8deee771ce5750e20dee03e72fd9bc38e3f4603
TitiTwister <thib26@hotmail.com>
2018-07-30 11:18:26 +0000

    fix ssh_operations for create_instances.py

--- create_instances.py
+++ create_instances.py
@@ -5,6 +5,9 @@ import boto3
 from login import ec2, client
 import json
 
+SALTMASTER = 'x.x.x.x'
+KEY_REPOSITORY = '/etc/salt/key/'
+
 def get_hosts(project):
     
     files = ['profiles', 'hosts']
@@ -51,11 +54,47 @@ def create_hosts(profiles, hosts):
 
     return 0
 
+def install_salt(profiles, hosts):
+
+    for key in hosts :
+        for i in range(0, hosts[key]["number"]):
+            hostname = hosts[key]["hostname"]+'-'+str(i+1)
+            instance_ip = hosts[key]["ip"][i]
+            key_path = KEY_REPOSITORY + 'outscale_' + profile["key"] + '.rsa'
+
+            if hosts[key]["salt"] == "minion" :
+                commands = ["sudo yum install -y salt-minion",
+                            "sudo chown centos:centos /etc/salt/minion",
+                            "echo -e 'id: %s\nmaster: %s\nhash_type: sha256' > /etc/salt/minion"%(hostname.lower(),SALTMASTER),
+                            "sudo systemctl enable salt-minion",
+                            "sudo systemctl start salt-minion"]
+                print("Connecting to : %s"%(instance_ip))
+                ssh_operations.ssh_commands(instance_ip, key_path, commands)
+                print("Instance : %s, %s salt-minion installed"%(hostname, instance_ip))
+    return 0
+
+def accept_salt_key(hosts):
+
+    for host in hosts :
+        hostname = hosts[host]["hostname"].lower() + '-*'
+        args = ['salt-key', '-a', hostname]
+        call = Popen(args, stdin=PIPE, stdout=PIPE)
+        call.communicate(input='Y')
+        print("Salt-key accepted for : %s"%(hostname))
+
+    return 0
+
+
 if __name__ == "__main__" :
     
     if len(sys.argv) < 2 or len(sys.argv) > 2:
         print("Wrong number of arguments, expected :\npython create_instances.py <project name> ")
 
     else :
+        print("Starting")
         profiles, hosts = get_hosts(sys.argv[1])
+        print("Profiles and hosts loaded")
         create_hosts(profiles, hosts)
+        install_salt(profiles, hosts)
+        accept_salt_key(hosts)
+
--- ssh_operations.py
+++ ssh_operations.py
@@ -2,7 +2,6 @@ import sys, paramiko
 
 def ssh_commands(hostname, key, commands, username='centos', password='nope', port=22):
     
-    commands = [ "ls /home/centos","ls /etc/salt"]
     ssh = paramiko.SSHClient()
 
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
